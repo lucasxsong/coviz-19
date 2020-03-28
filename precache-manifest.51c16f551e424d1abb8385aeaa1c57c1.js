027986534ccc75cfba8dfe2474fdd44653cddb5d
Lucas Song <lucas7song@gmail.com>
2020-03-27 17:10:53 -0700

    Updates

---
+++ precache-manifest.51c16f551e424d1abb8385aeaa1c57c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363dc0d12fb733a9e14662517246e517",
+    "url": "/coviz-19/index.html"
+  },
+  {
+    "revision": "7d0afcd84074f419b66c",
+    "url": "/coviz-19/static/css/main.cdaebc03.chunk.css"
+  },
+  {
+    "revision": "11473cc947490db5c8b4",
+    "url": "/coviz-19/static/js/2.38b89737.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coviz-19/static/js/2.38b89737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0afcd84074f419b66c",
+    "url": "/coviz-19/static/js/main.30a9d0da.chunk.js"
+  },
+  {
+    "revision": "40155b9cea51a485c5a9",
+    "url": "/coviz-19/static/js/runtime-main.787bba73.js"
+  }
+]);
